33b7224548f20bb1fadb80e3111b01cb35260d56
anh3112003qui <anh.qui0311@gmail.com>
2024-12-24 15:58:13 +0700

    new

---
+++ QuiLe_Hotel/src/main/java/com/anh3112003qui/quile_hotel/QuiLeHotelApplication.java
@@ -0,0 +1,13 @@
+package com.anh3112003qui.quile_hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuiLeHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuiLeHotelApplication.class, args);
+    }
+
+}
---
+++ QuiLe_Hotel/src/test/java/com/anh3112003qui/quile_hotel/QuiLeHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anh3112003qui.quile_hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuiLeHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
